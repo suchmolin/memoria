a621d03443d758406c6ead8d9ccd61b8acbdde83
suchmolin <suchmolin11@gmail.com>
2024-09-04 16:30:40 -0400

    change routes img

--- src/components/GameCards/index.jsx
+++ src/components/GameCards/index.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import "./index.css";
-import Cronometro from "../Cronometro";
-import FinalModal from "../FinalModal";
+import React from "react"
+import "./index.css"
+import Cronometro from "../Cronometro"
+import FinalModal from "../FinalModal"
 const participantes = [
   "wukong",
   "tristana",
@@ -19,60 +19,61 @@ const participantes = [
   "yi",
   "briar",
   "akali",
-];
-participantes.sort(() => Math.random() - 0.5);
+]
+participantes.sort(() => Math.random() - 0.5)
 
 const GameCards = (props) => {
-  const rutaIcon = "/memoria/src/assets/iconos/";
-  const [first, setFirst] = React.useState(false);
-  const [checking, setCheking] = React.useState(false);
-  const [oneSelected, setOneSelected] = React.useState([]);
-  const [aciertos, setAciertos] = React.useState(0);
-  const [errores, setErrores] = React.useState(0);
-  const [isGameOver, setIsGameOver] = React.useState(false);
-  const [seconds, setSeconds] = React.useState(0);
-  const [min, setMin] = React.useState(0);
+  const { nombre } = props
+  const [first, setFirst] = React.useState(false)
+  const [checking, setCheking] = React.useState(false)
+  const [oneSelected, setOneSelected] = React.useState([])
+  const [aciertos, setAciertos] = React.useState(0)
+  const [errores, setErrores] = React.useState(0)
+  const [isGameOver, setIsGameOver] = React.useState(false)
+  const [seconds, setSeconds] = React.useState(0)
+  const [min, setMin] = React.useState(0)
   const [posiciones, setPosiciones] = React.useState([])
 
   const selected = (e) => {
-    if (!first) setFirst(true);
+    if (!first) setFirst(true)
     if (!checking) {
-      e.target.classList.remove("oculto");
+      e.target.classList.remove("oculto")
       if (!oneSelected.length) {
-        setOneSelected([e.target.src, e.target.id]);
+        setOneSelected([e.target.src, e.target.id])
       } else {
         if (e.target.src !== oneSelected[0]) {
-          setCheking(true);
+          setCheking(true)
           setTimeout(() => {
-            e.target.classList.add("oculto");
-            document.getElementById(oneSelected[1]).classList.add("oculto");
-            setCheking(false);
-            setErrores(errores + 1);
-          }, 1000);
+            e.target.classList.add("oculto")
+            document.getElementById(oneSelected[1]).classList.add("oculto")
+            setCheking(false)
+            setErrores(errores + 1)
+          }, 1000)
         } else {
           if (aciertos + 1 === 8) {
-            setIsGameOver(true);
+            setIsGameOver(true)
             let array =
-              JSON.parse(window.localStorage.getItem("listaParticipantes")) ?? [];
+              JSON.parse(window.localStorage.getItem("listaParticipantes")) ??
+              []
 
             array.push({
-              name: props.nombre,
+              name: nombre,
               min: min,
               sec: seconds,
               error: errores,
-            });
+            })
             window.localStorage.setItem(
               "listaParticipantes",
               JSON.stringify(array)
-            );
+            )
             setPosiciones(array)
           }
-          setAciertos(aciertos + 1);
+          setAciertos(aciertos + 1)
         }
-        setOneSelected([]);
+        setOneSelected([])
       }
     }
-  };
+  }
 
   return (
     <>
@@ -85,7 +86,7 @@ const GameCards = (props) => {
           setSeconds={setSeconds}
           min={min}
           setMin={setMin}
-          />
+        />
         {participantes.map((carta, i) => {
           return (
             <div className="card" key={i}>
@@ -93,31 +94,29 @@ const GameCards = (props) => {
                 className="oculto"
                 id={`img${i}`}
                 onClick={selected}
-                src={`${rutaIcon}${carta}.jpg`}
+                src={`/iconos/${carta}.jpg`}
                 alt=""
-                />
+              />
             </div>
-          );
+          )
         })}
 
         <div className="w-full font-Lobster text-2xl mt-3 text-center">
           <p>{aciertos} de 8</p>
         </div>
       </div>
-      {
-        isGameOver &&
-         <FinalModal
-            nombre={props.nombre}
-            min={min}
-            seconds={seconds}
-            errores={errores}
-            isGameOver={isGameOver}
-            posiciones={posiciones}
-          /> 
-
-      }
+      {isGameOver && (
+        <FinalModal
+          nombre={nombre}
+          min={min}
+          seconds={seconds}
+          errores={errores}
+          isGameOver={isGameOver}
+          posiciones={posiciones}
+        />
+      )}
     </>
-  );
-};
+  )
+}
 
-export default GameCards;
+export default GameCards
--- src/components/bienvenida/index.jsx
+++ src/components/bienvenida/index.jsx
@@ -1,43 +1,52 @@
-import React from "react";
-import './index.css';
+import React from "react"
+import "./index.css"
 
+const Bienvenida = (props) => {
+  const { nombre, setNombre, setIniciarJuego } = props
 
-const Bienvenida =  (props) => {
-    const {
-        nombre,
-        setNombre,
-        setIniciarJuego
-    } = props
-    
-    
+  const iniciar = (e) => {
+    e.preventDefault()
+    const nombre = document.getElementById("nombreJugador").value
+    setNombre(nombre)
+    if (nombre === "") return
+    setIniciarJuego(true)
+  }
 
-    const iniciar = (e) => {
-        e.preventDefault()
-        const nombre = document.getElementById("nombreJugador").value
-        setNombre(nombre)
-        if (nombre === '') return;
-        setIniciarJuego(true)
-    }
-
-    return (
-        <>
-            <header className=" mx-12 mt-10 relative h-32 text-white opacity-4">
-                <h1 className="pt-9 xs:pt-2 text-center text-4xl md:text-5xl font-Lobster">Bienvenidos a Memoria by JoseS</h1>
-            </header>
-            <section className="h-3/6 flex items-center flex-col">
-                <img width="700px" className="" src="memoria/src/assets/juego.png" alt="" />
-                <div className="flex w-full text-white">
-                    <form action="" className="flex flex-row justify-center gap-6 w-full">
-                        <label htmlFor="nombreJugador" className="block text-sm font-medium leading-6font-Lobster">Nombre</label>
-                        <input id="nombreJugador"type="text" required className="block w-30 rounded-md border-0 py-1 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-inset sm:text-sm sm:leading-6"/>
-                        <button onClick={iniciar} type="submit" className="rounded-md bg-sky-300 px-3 py-1.5 text-sm font-Lobster shadow-sm hover:bg-sky-500 focus-visible:outline ">Comenzar</button>
-                    </form>
-                </div>
-            </section>
-
-        </>
-
-    )
+  return (
+    <>
+      <header className=" mx-12 mt-10 relative h-32 text-white opacity-4">
+        <h1 className="pt-9 xs:pt-2 text-center text-4xl md:text-5xl font-Lobster">
+          Bienvenidos a Memoria by JoseS
+        </h1>
+      </header>
+      <section className="h-3/6 flex items-center flex-col">
+        <img width="700px" className="" src="/juego.png" alt="" />
+        <div className="flex w-full text-white">
+          <form action="" className="flex flex-row justify-center gap-6 w-full">
+            <label
+              htmlFor="nombreJugador"
+              className="block text-sm font-medium leading-6font-Lobster"
+            >
+              Nombre
+            </label>
+            <input
+              id="nombreJugador"
+              type="text"
+              required
+              className="block w-30 rounded-md border-0 py-1 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-inset sm:text-sm sm:leading-6"
+            />
+            <button
+              onClick={iniciar}
+              type="submit"
+              className="rounded-md bg-sky-300 px-3 py-1.5 text-sm font-Lobster shadow-sm hover:bg-sky-500 focus-visible:outline "
+            >
+              Comenzar
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
+  )
 }
 
-export default Bienvenida;
+export default Bienvenida
